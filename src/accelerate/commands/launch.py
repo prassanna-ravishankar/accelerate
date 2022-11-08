e431c96c73a55acd1008a9e744056503d2d0c210
muellerzr <muellerzr@gmail.com>
2022-11-07 20:19:39 -0500

    args

--- src/accelerate/commands/launch.py
+++ src/accelerate/commands/launch.py
@@ -836,7 +836,7 @@ def tpu_pod_launcher(args):
         args, xla_dist.get_args_parser(), ["--tpu", args.tpu_name, "--positional", "", "--restart-tpuvm-pod-server"]
     )
     new_args.positional = ["accelerate", "launch"]
-    print(args.__dict__)
+    print(dir(args))
     raise ValueError()
     # for arg in vars(args):
 
