56be9a2460378454c7dec347988dea5ec51b38f8
muellerzr <muellerzr@gmail.com>
2022-11-07 22:49:25 -0500

    Specify worker_config

--- src/accelerate/commands/launch.py
+++ src/accelerate/commands/launch.py
@@ -836,7 +836,7 @@ def tpu_pod_launcher(args):
         args, xla_dist.get_args_parser(), ["--tpu", args.tpu_name, "--positional", "", "--restart-tpuvm-pod-server"]
     )
 
-    new_args.positional = ["accelerate", "launch", training_script] + training_script_args
+    new_args.positional = ["accelerate", "launch", "--config_file", "worker_config.yaml", training_script] + training_script_args
     xrt_config = current_env.pop("XRT_TPU_CONFIG")
     bad_flags = ""
     for arg in vars(new_args):
